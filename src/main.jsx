068807650e2ff5ff9aac9433178cbecb55517a74
niv <nivroz93@gmail.com>
2021-08-01 10:53:31 +0300

    added jump to top

--- src/main.jsx
+++ src/main.jsx
@@ -131,3 +131,11 @@ function animate() {
 }
 
 animate();
+$(window).scroll(function () {
+  if ($(this).scrollTop() >= 50) {
+    // If page is scrolled more than 50px
+    $("#return-to-top").fadeIn(200); // Fade in the arrow
+  } else {
+    $("#return-to-top").fadeOut(200); // Else fade out the arrow
+  }
+});
